4bc1cdddd478b087d6f0b944c4958beaf3d18778
Dina Marcaida Fajardo <fajardod91@yahoo.com.ph>
2014-12-06 22:26:50 +0800

    kana-files

---
+++ src/kana/Kana.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kana;
+
+/**
+ *
+ * @author kienah
+ */
+public class Kana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        kanaGui form = new kanaGui();
+        form.setVisible (true);  
+    }
+}
